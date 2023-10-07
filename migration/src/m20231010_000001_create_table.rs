8392bd93920de00594564dc9fe8979e5bee3d2a0
Marcel <me@m4rc3l.de>
2023-10-07 20:36:58 +0200

    updated migrations

--- migration/src/m20231010_000001_create_table.rs
+++ migration/src/m20231010_000001_create_table.rs
@@ -10,18 +10,64 @@ impl MigrationTrait for Migration {
   async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
     let db = manager.get_connection();
 
-    // Use `execute_unprepared` if the SQL statement doesn't have value bindings
+    // TODO: CAA, SRV
+
     db.execute_unprepared(
-      "create table zone (
-                id      uuid primary key,
-                created timestamptz  not null default current_timestamp,
-                admin   varchar(255) not null,
-                name    varchar(255) not null,
-                refresh integer      not null,
-                retry   integer      not null,
-                expire  integer      not null,
-                minimum integer      not null
-            );",
+      "
+      create table zone
+      (
+          id      uuid primary key,
+          created timestamptz  not null,
+          updated timestamptz  not null,
+          admin   varchar(255) not null,
+          name    varchar(255) not null,
+          refresh integer      not null,
+          retry   integer      not null,
+          expire  integer      not null,
+          minimum integer      not null
+      );
+
+      create table record
+      (
+          id      uuid primary key,
+          created timestamptz  not null,
+          updated timestamptz  not null,
+          name    varchar(255) not null,
+          zone_id uuid         not null references zone (id),
+          ttl     integer      not null
+      );
+
+      create table record_a
+      (
+          id      uuid primary key references record(id),
+          address varchar(15)  not null
+      );
+
+      create table record_aaaa
+      (
+          id      uuid primary key references record(id),
+          address varchar(41)  not null
+      );
+
+      create table record_cname
+      (
+          id      uuid primary key references record(id),
+          target  varchar(255) not null
+      );
+
+      create table record_mx
+      (
+          id       uuid primary key references record(id),
+          target   varchar(255) not null,
+          priority smallint     not null
+      );
+
+      create table record_txt
+      (
+          id       uuid primary key references record(id),
+          content  text         not null
+      );
+    ",
     )
     .await?;
 
@@ -31,7 +77,16 @@ impl MigrationTrait for Migration {
   async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
     manager
       .get_connection()
-      .execute_unprepared("DROP TABLE `zone`")
+      .execute_unprepared(
+        "
+        DROP TABLE zone;
+        DROP TABLE record_a;
+        DROP TABLE record_aaaa;
+        DROP TABLE record_cname;
+        DROP TABLE record_mx;
+        DROP TABLE record_txt;
+      ",
+      )
       .await?;
 
     Ok(())
