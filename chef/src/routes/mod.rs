13511876307d2774176413f3bf646a5a6a42ac9c
revol-xut <revol-xut@protonmail.com>
2023-10-08 12:39:12 +0200

    adding all other methods for zones

--- chef/src/routes/mod.rs
+++ chef/src/routes/mod.rs
@@ -1,15 +1,18 @@
-use axum::routing::{get, post};
+use axum::routing::{delete, get, post, put};
 use axum::Router;
 
 pub mod zone;
 
 use crate::state::ChefState;
 
-use crate::routes::zone::list_zones;
+use crate::routes::zone::{delete_zone, get_zone, list_zones, modify_zone};
 use zone::create_zone;
 
 pub(crate) fn route() -> Router<ChefState> {
   Router::new()
     .route("/v1/zone", get(list_zones))
     .route("/v1/zone", post(create_zone))
+    .route("/v1/zone/:zone_id", put(modify_zone))
+    .route("/v1/zone/:zone_id", delete(delete_zone))
+    .route("/v1/zone/:zone_id", get(get_zone))
 }
--- chef/src/routes/zone.rs
+++ chef/src/routes/zone.rs
@@ -43,15 +43,35 @@ pub(crate) async fn create_zone(
 
 pub(crate) async fn modify_zone(
   State(mut state): State<ChefState>,
-  Path(id): Path<Uuid>,
+  Path(zone_id): Path<Uuid>,
   Json(payload): Json<ZoneRequest>,
 ) -> StatusCode {
   if !payload.valid_email() {
     return StatusCode::BAD_REQUEST;
   }
 
-  match state.zone_service.update(id, payload).await {
+  match state.zone_service.update(zone_id, payload).await {
     Ok(_) => StatusCode::OK,
     Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+  }
+}
+
+pub(crate) async fn delete_zone(
+  State(mut state): State<ChefState>,
+  Path(zone_id): Path<Uuid>,
+) -> StatusCode {
+  match state.zone_service.delete(zone_id).await {
+    Ok(_) => StatusCode::OK,
+    Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+  }
+}
+
+pub(crate) async fn get_zone(
+  State(mut state): State<ChefState>,
+  Path(zone_id): Path<Uuid>,
+) -> Result<Json<Arc<zone::Model>>, StatusCode> {
+  match state.zone_service.find(zone_id).await {
+    Ok(value) => value.ok_or(StatusCode::NOT_FOUND).map(Arc::new).map(Json),
+    Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
   }
 }
